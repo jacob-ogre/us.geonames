30c01b52444e51f44162ff27e98c6e34f2b0674d
Jacob Malcom <jmalcom@defenders.org>
2017-08-30 08:37:40 -0400

    Add more to batch script.

--- inst/scripts/batch_5yr.R
+++ inst/scripts/batch_5yr.R
@@ -6,8 +6,10 @@ library(stringr)
 library(us.geonames)
 
 init()
+load("geonames.lite.rda")
 
 files <- list.files("~/Downloads/five_year_review/",
+# files <- list.files("~/Data/ESAdocs_text/five_year_review/",
                     full.names = TRUE,
                     recursive = TRUE)
 files_df <- data_frame(
@@ -16,7 +18,8 @@ files_df <- data_frame(
 )
 
 # Get 5y rev data table:
-load("/Users/jacobmalcom/Downloads/ECOS_species_tables_2016-12-17.rda")
+# load("/Users/jacobmalcom/Downloads/ECOS_species_tables_2016-12-17.rda")
+load("ECOS_species_tables_2016-12-17.rda")
 head(fiveyr_table)
 fiveyr_table$doc <- basename(fiveyr_table$Doc_Link) %>%
   str_replace_all("pdf$", "txt")
@@ -32,44 +35,54 @@ mm <- as_data_frame(mult)
 ggplotly(qplot(data = mm, x = value, geom = "histogram", bins = 9))
 
 # Get counties data and prep
-load("/Users/jacobmalcom/Work/Data/esadocs/rda/counties_table_2016-12-10.rda")
+# load("/Users/jacobmalcom/Work/Data/esadocs/rda/counties_table_2016-12-10.rda")
+load("counties_table_2016-12-10.rda")
 names(counties_table)
 states <- data_frame(state_alpha = state.abb, State = state.name)
-cnt_occ <- as_tibble(left_join(counties_table, states, by = "State"))
+# cnt_occ <- as_tibble(left_join(counties_table, states, by = "State"))
+cnt_occ <- readr::read_tsv("~/Downloads/EndSp_county_occurrences.tsv")
 
 run_gn_search <- function(file) {
+  NAs <- data_frame(
+    "terms" = NA, "match" = NA, "feature_id" = NA, "feature_class" = NA,
+    "state_alpha" = NA, "state_numeric" = NA, "county_name" = NA, 
+    "county_numeric" = NA, "prim_lat_dec" = NA, "prim_long_dec" = NA, 
+    "elev_in_m" = NA, "elev_in_ft" = NA, "map_name" = NA, 
+    "date_created" = NA, "date_edited" = NA, "species" = NA, "file" = file
+  )
   cat("\n\n")
   cat(paste("Processing:", file, "\n"))
   f2 <- file
   sub_dat <- filter(fiveyr, doc == file) %>% distinct(doc, .keep_all = TRUE)
   fil_dat <- filter(files_df, doc == f2)
   data <- left_join(fil_dat, sub_dat, by = "doc")
-  # cat("data:\n")
-  # print(dim(data))
-  cos <- filter(cnt_occ, cnt_occ$Scientific_Name == data$Species)
-  cos_ls <- vector("list", dim(cos)[1])
-  for(i in 1:dim(cos)[1]) {
-    cos_ls[[i]] <- c(cos$state_alpha[i], cos$County[i])
-  }
+  print(dim(data))
+  cos <- filter(cnt_occ, cnt_occ$SCI == data$Species)
+  cat("\n\n")
+  cat(paste("Processing:", data$Species, "\n"))
 
-  # search =======================
-  text <- gn_read_txt(fil_dat$file)
-  srch <- gn_search_county(text, cos_ls)
-  if(dim(srch)[1] > 0) {
-    srch$species <- data$Species
-    srch$file <- file
-    map <- us.geonames::gn_leaflet_basic(srch)
+  if(dim(cos)[1] > 0) {
+    cos_ls <- vector("list", dim(cos)[1])
+    for(i in 1:dim(cos)[1]) { cos_ls[[i]] <- c(cos$STABBREV[i], cos$NAME[i]) }
+    text <- gn_read_txt(fil_dat$file)
+    srch <- gn_search_county(text, cos_ls)
+    if(dim(srch)[1] > 0) {
+      srch$species <- data$Species
+      srch$file <- file
+      map <- us.geonames::gn_leaflet_basic(srch)
+    } else {
+      srch <- NAs
+      srch$species <- data$Species
+      map <- NA
+    }
   } else {
-    srch <- data_frame(
-      "terms" = NA, "match" = NA, "feature_id" = NA, "feature_class" = NA,
-      "state_alpha" = NA, "state_numeric" = NA, "county_name" = NA, 
-      "county_numeric" = NA, "prim_lat_dec" = NA, "prim_long_dec" = NA, 
-      "elev_in_m" = NA, "elev_in_ft" = NA, "map_name" = NA, 
-      "date_created" = NA, "date_edited" = NA, "species" = NA, "file" = file
-    )
+    srch <- NAs
     map <- NA
+    cos_ls <- list(NA)
+    text <- gn_read_txt(fil_dat$file)
   }
-  return(list(species = data$Species,
+  return(list(file = file,
+              species = data$Species,
               found = srch,
               map = map,
               st_co = cos_ls,
@@ -77,7 +90,8 @@ run_gn_search <- function(file) {
 }
 
 bind_all_res <- function(res_list) {
-  dfs <- lapply(res_list, FUN = `[[`, 1)
+  # dfs <- lapply(res_list, FUN = `[[`, 1)
+  dfs <- lapply(res_list, FUN = `[[`, 3)
   res <- bind_rows(dfs)
   return(res)
 }
@@ -87,27 +101,38 @@ ot <- lapply(ts, FUN = run_gn_search)
 
 ot_all <- bind_all_res(ot)
 
-ts <- files_df$doc[26:50]
+# ts <- files_df$doc[1:25]
+ts <- files_df$doc[1:100]
 ot <- parallel::mclapply(
-# ot <- lapply(
   ts, 
   FUN = run_gn_search,
-  mc.cores = 3,
+  mc.cores = 4,
   mc.preschedule = FALSE
 )
 
-ot_all <- bind_all_res(ot)
-brun <- ot[[19]]$found
-brun_nat <- gn_natural_places(brun)
-gn_leaflet_basic(brun_nat)
+ts2 <- files_df$doc[101:length(files_df$doc)]
+ot2 <- parallel::mclapply(
+  ts2, 
+  FUN = run_gn_search,
+  mc.cores = 4,
+  mc.preschedule = FALSE
+)
 
-tally <- sort(table(brun$terms), decreasing = TRUE)
-good <- tally[tally > 2]
-brun_2 <- filter(brun, terms %in% names(good))
-gn_leaflet_basic(brun_2)
+ts3 <- files_df$doc[1:100]
+ot3 <- parallel::mclapply(
+  ts3, 
+  FUN = run_gn_search,
+  mc.cores = 4,
+  mc.preschedule = FALSE
+)
 
+ts4 <- files_df$doc[101:length(files_df$doc)]
+ot4 <- parallel::mclapply(
+  ts4, 
+  FUN = run_gn_search,
+  mc.cores = 4,
+  mc.preschedule = FALSE
+)
 
-
-
-
+counties <- readr::read_tsv("~/Downloads/EndSp_county_occurrences.tsv")
 
