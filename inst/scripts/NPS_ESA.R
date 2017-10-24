94b2f8b5a8adb415c8c351312685a545e6aabf09
Jacob Malcom <jmalcom@defenders.org>
2017-10-24 14:36:08 -0400

    Add script to pull national parks from ESA docs.

--- inst/scripts/NPS_ESA.R
+++ inst/scripts/NPS_ESA.R
@@ -17,10 +17,29 @@ con <- dbConnect(
   host = ESC_DB
 )
 
+# con <- dbConnect(
+#   dbDriver("PostgreSQL"),
+#   dbname = "esc-dev",
+#   user = "postgres",
+#   password = "SillySilly1!",
+#   host = "localhost"
+# )
+
+
 fyr <- dbSendQuery(con, "select * from ecos_fiveyr_review_docs") %>% dbFetch()
 fyr$doc_file <- basename(fyr$doc_link) %>%
   str_replace("pdf$|PDF$", "txt")
 
+rp <- dbSendQuery(con, "select * from ecos_recovery_plan_docs") %>% dbFetch()
+rp$doc_file <- basename(rp$doc_link) %>%
+  str_replace("pdf$|PDF$", "txt")
+
+fr <- dbSendQuery(con, "select * from ecos_fed_reg_docs") %>% dbFetch()
+fr$doc_file <- basename(fr$doc_link) %>%
+  str_replace("pdf$|PDF$", "txt")
+
+###########################
+# Now geonames and NPS data
 data(geonames.lite, package = "us.geonames")
 nps <- read_csv("data/nps_boundary.csv")
 
@@ -48,13 +67,27 @@ get_nps_places <- function(text) {
 }
 
 fiveyr_fils <- list.files(
-  "~/Downloads/five_year_review",
+  # "~/Downloads/five_year_review",
+  "~/Work/Data/ESAdocs_text/five_year_review",
   full.names = TRUE,
   recursive = TRUE
 )
 
-# nps_res1 <- lapply(fiveyr_fils[1:100], FUN = function(fil) {
-nps_res1 <- lapply(fiveyr_fils, FUN = function(fil) {
+rp_fils <- list.files(
+  "~/Work/Data/ESAdocs_text/recovery_plan",
+  full.names = TRUE,
+  recursive = TRUE
+)
+
+fr_fils <- list.files(
+  "~/Work/Data/ESAdocs_text/federal_register",
+  full.names = TRUE,
+  recursive = TRUE
+)
+
+##########################################################################
+# five-year reviews first
+nps_res1 <- lapply(fiveyr_fils, fun = function(fil) {
   txt <- gn_read_txt(fil)
   n_words <- str_split(txt, " ") %>% unlist() %>% length()
   res <- get_nps_places(txt)
@@ -74,6 +107,85 @@ table(nps_spp_filt$feature_name) %>% sort(decreasing = TRUE) %>% head(10)
 unique(nps_spp_filt$feature_name) %>% length()
 unique(nps_spp_filt$species) %>% length()
 
+#######################################################################
+# recovery plans
+nps_rp1 <- parallel::mclapply(
+  rp_fils,
+  mc.cores = 3,
+  mc.preschedule = FALSE,
+  FUN = function(fil) {
+    txt <- gn_read_txt(fil)
+    n_words <- str_split(txt, " ") %>% unlist() %>% length()
+    res <- get_nps_places(txt)
+    if(dim(res)[1] > 0) {
+      res$doc_path <- fil
+      res$doc_file <- basename(fil)
+      res$doc_length <- n_words
+    }
+    return(res)
+})
 
+nps_rp2 <- bind_rows(nps_rp1)
+nps_spp_rp <- left_join(nps_rp2, rp, by = "doc_file")
+nps_spp_rp_filt <- filter(nps_spp_rp,
+                       n_primary > 1 | (n_primary == 1 & n_secondary > 1))
+table(nps_spp_rp_filt$feature_name) %>% sort(decreasing = TRUE) %>% head(10)
+unique(nps_spp_rp_filt$feature_name) %>% length()
+unique(nps_spp_rp_filt$species) %>% length()
+nps_spp_rp_filt <- select(nps_spp_rp_filt, -plan_action_status, -plan_status)
 
+#######################################################################
+# Fed Reg
+nps_fr1 <- parallel::mclapply(
+  fr_fils,
+  mc.cores = 3,
+  mc.preschedule = FALSE,
+  FUN = function(fil) {
+    txt <- gn_read_txt(fil)
+    n_words <- str_split(txt, " ") %>% unlist() %>% length()
+    res <- get_nps_places(txt)
+    if(dim(res)[1] > 0) {
+      res$doc_path <- fil
+      res$doc_file <- basename(fil)
+      res$doc_length <- n_words
+    }
+    return(res)
+})
 
+nps_fr2 <- bind_rows(nps_fr1)
+nps_spp_fr <- left_join(nps_fr2, fr, by = "doc_file")
+nps_spp_fr_filt <- filter(nps_spp_fr,
+                       n_primary > 1 | (n_primary == 1 & n_secondary > 1))
+table(nps_spp_fr_filt$feature_name) %>% sort(decreasing = TRUE) %>% head(10)
+unique(nps_spp_fr_filt$feature_name) %>% length()
+unique(nps_spp_fr_filt$species) %>% length()
+nps_spp_fr_filt <- select(nps_spp_fr_filt, -citation_page)
+
+# Side-tracked
+fyr_rp_combo <- rbind(nps_spp_filt, nps_spp_rp_filt)
+table(fyr_rp_combo$feature_name) %>% sort(decreasing = TRUE) %>% head(10)
+unique(fyr_rp_combo$feature_name) %>% length()
+unique(fyr_rp_combo$species) %>% length()
+
+sm_fyr_rp <- select(fyr_rp_combo, c(2,8:10,15:17,19,21:24)) %>%
+  distinct(feature_name, species, .keep_all = TRUE)
+rio::export(sm_fyr_rp, file = "~/Downloads/NPS_places.xlsx")
+
+# After manual cleanup...
+NPS_dat <- readxl::read_excel("~/Downloads/NPS_places_ESA.xlsx")
+unique(NPS_dat$species) %>% length()
+unique(NPS_dat$feature_name) %>% length()
+
+# now back to adding hits from FR
+sm_fr <- select(nps_spp_fr_filt, c(2,8:10,15:17,19,21:24)) %>%
+  distinct(feature_name, species, .keep_all = TRUE)
+fyr_rp_fr_combo <- rbind(NPS_dat, sm_fr)
+fyr_rp_fr_filt <- filter(fyr_rp_fr_combo,
+                         !grepl(fyr_rp_fr_combo$title,
+                               pattern = "Review of Native Species") &
+                         !grepl(fyr_rp_fr_combo$title,
+                               pattern = "Listing 38 Species on Molokai"))
+
+unique(fyr_rp_fr_filt$feature_name) %>% length()
+unique(fyr_rp_fr_filt$species) %>% length()
+rio::export(fyr_rp_fr_filt, "~/Downloads/NPS_fyr_rp_fr.xlsx")
